cf77cfc72187c2379714d8c1755448b0bafa657d
unknown <josephgenescar@gmail.com>
2026-05-26 20:32:43 -0400

    bbb

--- build-supabase-config.js
+++ build-supabase-config.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const env = {
-  SUPABASE_URL: process.env.SUPABASE_URL || '',
-  SUPABASE_KEY: process.env.SUPABASE_KEY || '',
-  SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY || ''
+  SUPABASE_URL: process.env.SUPABASE_URL || 'https://kcldctdalowqkwhlhcdy.supabase.co',
+  SUPABASE_KEY: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbGRjdGRhbG93cWt3aGxoY2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTYwNzYsImV4cCI6MjA5MzI5MjA3Nn0.iX3hswYAeYffQlpBp2N7AcSz-6ycrYDQSldTkmmvXxg',
+  SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbGRjdGRhbG93cWt3aGxoY2R5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzcxNjA3NiwiZXhwIjoyMDkzMjkyMDc2fQ.5IsbCAJbnyJupKqMuJMfoiINeB6dubmUC_A8IVYY9q4'
 };
 
 const filePath = path.join(__dirname, 'supabase-config.js');
