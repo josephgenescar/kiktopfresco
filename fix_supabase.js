2c6380e9761ba6a555b7fa205c4117374cea99c9
unknown <josephgenescar@gmail.com>
2026-05-27 10:29:24 -0400

    bb

--- fix_supabase.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('index.html', 'utf8');
-const newContent = content.replace('SUPABASE_URL !== "https://kcldctdalowqkwhlhcdy.supabase.co"', 'SUPABASE_URL');
-fs.writeFileSync('index.html', newContent);
-console.log('Fixed Supabase initialization condition');
