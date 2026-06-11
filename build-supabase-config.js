24c75cbc5ddd7c74c526e809c3f847f71d8569d4
unknown <josephgenescar@gmail.com>
2026-06-11 11:49:54 -0400

    bb

--- build-supabase-config.js
+++ build-supabase-config.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const env = {
   SUPABASE_URL: process.env.SUPABASE_URL || 'https://kcldctdalowqkwhlhcdy.supabase.co',
-  SUPABASE_KEY: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbGRjdGRhbG93cWt3aGxoY2R5Iiwicm9zYSI6ImFub24iLCJpYXQiOjE3Nzc3MTYwNzYsImV4cCI6MjA5MzI5MjA3Nn0.iX3hswYAeYffQlpBp2N7AcSz-6ycrYDQSldTkmmvXxg',
-  SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZWsiLCJyZWYiOiJrY2xkY3RkYWxvd2txd2hsY2hkeSIsInJvbGUiOiJzZXJ2aWNlX3JvbGUiLCJpYXQiOjE3Nzc3MTYwNzYsImV4cCI6MjA5MzI5MjA3Nn0.5IsbCAJbnyJupKqMuJMfoiINeB6dubmUC_A8IVYY9q4'
+  SUPABASE_KEY: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbGRjdGRhbG93cWt3aGxoY2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTYwNzYsImV4cCI6MjA5MzI5MjA3Nn0.iX3hswYAeYffQlpBp2N7AcSz-6ycrYDQSldTkmmvXxg',
+  SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbGRjdGRhbG93cWt3aGxoY2R5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzcxNjA3NiwiZXhwIjoyMDkzMjkyMDc2fQ.5IsbCAJbnyJupKqMuJMfoiINeB6dubmUC_A8IVYY9q4'
 };
 
 const imagekit = {
